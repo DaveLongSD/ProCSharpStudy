ba9f9c7a864e4ff5fa75fc8d057f5cda2edecc17
DaveLongSD <davelong.sd1@gmail.com>
2015-05-20 11:51:04 -0400

    initial commit

---
+++ SpecifyingAnApplicationErrorCode/SpecifyingAnApplicationErrorCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecifyingAnApplicationErrorCode
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("***** My First C# App ***** ");
+            Console.WriteLine("Hello World");
+            Console.WriteLine();
+
+            //Wait for Enter Key to be pressed before shutting down.
+            Console.ReadLine();
+
+            return -1;
+        }
+    }
+}
